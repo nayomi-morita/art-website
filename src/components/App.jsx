0d88ffd0b350975356914018895399551b3f5872
Nayomi Morita <nayomi.morita@gmail.com>
2019-05-03 09:47:46 -0700

    add app routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,17 @@ import { Switch, Route, Link } from 'react-router-dom';
 import Error404 from './Error404';
 import Header from './Header';
 import Home from './Home';
+import Admin from './Admin';
 
 function App(){
   return(
     <div>
-    
+    <Header/>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/Admin' component={Admin} />
+        <Route component={Error404} />
+      </Switch>
     </div>
   );
 }
