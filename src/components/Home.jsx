4c37c011d4b1e8f62976497a379db7b07c152fb7
Nayomi Morita <nayomi.morita@gmail.com>
2019-05-03 11:01:00 -0700

    add state display in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Switch, Route, Link } from 'react-router-dom';
+import artReducer from './../reducers/art-reducer.js';
+import { connect } from 'react-redux';
 
-function Home() {
+function Home(props) {
   return(
     <div>
       <p>Test</p>
-      <p></p>
-      <p></p>
+      <p>{props.artReducer.title}</p>
+      <p>{props.artReducer.description}</p>
     </div>
   );
 }
+
+const mapStateToProps = state => {
+  return {
+    artReducer: state
+  }
+}
+
+Home = connect(mapStateToProps)(Home);
 
 export default Home;
