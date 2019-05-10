1233bb18192fe4b068d9aee592ba9e57495ccbf7
Nayomi Morita <nayomi.morita@gmail.com>
2019-05-10 13:56:56 -0700

    add spacing to pages

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,11 @@ import { Switch, Route, Link } from 'react-router-dom';
 function Contact() {
   return(
     <div>
+      <style jsx>{`
+        div{
+          padding: 0 20px 0 20px;
+        }
+      `}</style>
       <p>Contact</p>
     </div>
   );
